629accc287b1b89ba9de6d3110b7eeb5662cdac1
Dylan McFerran <Dylan McFerran@10.253.16.92>
2017-02-25 15:01:04 -0500

    Added Inventory and Product classes

---
+++ Biz-Ignite/Inventory.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Inventory {
+
+	//fields
+	
+	private ArrayList<Product> inventory = new ArrayList<Product>();
+	
+	//constructors
+	
+	public Inventory() {
+		inventory = null;
+	}
+	
+	//methods
+	
+	
+}
